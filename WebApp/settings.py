69105bb794f4f7e27d9a88fa13db2ba1c7694b5f
Paul Hahn <paulhahn18@gmail.com>
2020-10-27 10:18:34 -0400

    Get secret key from environment variable.

--- WebApp/settings.py
+++ WebApp/settings.py
@@ -11,16 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^li5ar6@8sctj021zj6!x47%!t)(!tqlrv9em3fqyoa(bmu4(_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
