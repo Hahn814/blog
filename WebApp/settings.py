30211dc81140b24275c8163cddda125b9deed20e
Paul Hahn <pbhahn@outlook.com>
2021-07-22 18:48:18 -0400

    config: Add hostname

--- WebApp/settings.py
+++ WebApp/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'web', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'web', 'localhost', 'pbhahn.xyz']
 
 
 # Application definition
